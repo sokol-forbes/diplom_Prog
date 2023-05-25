7a021d72d20bdaa07d03430405528a282285fd1d
Aliaksandr Kliutsuk <Lait-v.0.1@yandex.ru>
2023-05-25 17:27:42 +0300

    Create directory documentation and create spring

---
+++ src/main/java/com/diplom/server/dao/entity/Trainings.java
@@ -0,0 +1,16 @@
+package com.diplom.server.dao.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString(callSuper = true)
+public class Trainings {
+
+}
